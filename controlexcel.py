453079571b9e80f4adf8363506b13fd32b34b1c7
Gonzalo Astudillo Ortega <astudillogonzalo99@gmail.com>
2022-07-25 14:46:38 +0200

    hecho

--- controlexcel.py
+++ controlexcel.py
@@ -1,15 +1,16 @@
+from googleapiclient.discovery import build
+from google.oauth2 import service_account
 import requests
 from openpyxl import Workbook
 import numpy as np
 import pandas as pd
-import xlsxwriter
 
-stage = "https://backend.stage.zinkee.com:8443/MasaBackend-stage"
+stage = "https://www.backend.stage.zinkee.com:8443/MasaBackend-stage"
 apiRegistros = "/api/mant/registro/list"
 apiCampos = "/api/mant/skeleton/get"
-app = "65"
-apiKey = "3B35DEBE-79B8-4CFE-AD8A-E95F2CEA2783"
-math = "31"
+app = "116"
+apiKey = "5C10640B-9EF6-4B86-811A-68DF8A6E0EDE"
+math = "19"
 
 #Variable request mannt/registro
 registros = []
@@ -19,8 +20,6 @@ Campos = []
 VectorTipos = []
 VectorId = []
 VectorNombre= []
-
-x = [[]]
 
 if __name__ == '__main__':
     
@@ -49,7 +48,7 @@ if __name__ == '__main__':
             
             if((VectorTipos[n] == "T") or (VectorTipos[n] == "F") or (VectorTipos[n] == "H")or (VectorTipos[n] == "B") or (VectorTipos[n] == "V")):
                 Campos.append(reg["data"][n]["valor"])
-            elif((VectorTipos[n] == "N") or (VectorTipos[n] == "D")):
+            elif((VectorTipos[n] == "N")):
                 Campos.append(reg["data"][n]["valor"]["numberFixed"])
             elif((VectorTipos[n] == "U") or (VectorTipos[n] == "LV") or (VectorTipos[n] == "UG") or (VectorTipos[n] == "RM")):
                 Campos.append(reg["data"][n]["valorRel"])
@@ -59,12 +58,9 @@ if __name__ == '__main__':
     VCampos = np.array(Campos)
     MCampos = np.reshape(VCampos, (len(registros), len(VectorTipos)))
     
-    print(MCampos)
-    print(VectorNombre)
-    print(registros)
-    print(Campos)
-    
-    archivo = xlsxwriter.Workbook("porfavor.xlsx")
+    '''''
+    #xlsx libreria
+    archivo = xlsxwriter.Workbook("zinkeetabla.xlsx")
     hoja = archivo.add_worksheet()
     
     for n in range(0, len(VectorNombre)):
@@ -74,6 +70,35 @@ if __name__ == '__main__':
         for col in range(0,len(VectorNombre)):
             hoja.write(row,col,MCampos[row][col])
             
-    
     archivo.close()
+    '''''
     
+    #API GOOGLE SHEET
+    
+    SERVICE_ACCOUNT_FILE = 'keys.json'
+    SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
+
+    creds = None
+    creds = service_account.Credentials.from_service_account_file(
+            SERVICE_ACCOUNT_FILE, scopes=SCOPES)
+
+    # ID = ((URL spreadsheet - */d) - /edit*)
+    # The ID spreadsheet.
+    SAMPLE_SPREADSHEET_ID = '1lRIDShBQU1oe7WOi6Q3FeuOCBvrAp02-A3_LhgwZ6Zs'
+    SAMPLE_RANGE_NAME = 'test!A1'
+
+    service = build('sheets', 'v4', credentials=creds)
+    sheet = service.spreadsheets()
+    
+    nameCampos = [VectorNombre]
+    request=sheet.values().update(spreadsheetId=SAMPLE_SPREADSHEET_ID,
+                                 range="test!A1",valueInputOption="USER_ENTERED",body={"values":nameCampos}).execute()
+    content = MCampos.tolist()
+    request=sheet.values().update(spreadsheetId=SAMPLE_SPREADSHEET_ID,
+                                 range="test!A2",valueInputOption="USER_ENTERED",body={"values":content}).execute()
+    
+    result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
+                            range="test!A:Z").execute()
+
+    values = result.get('values', [])
+    print(values)
--- tutorialpanchito.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import requests 
-import openpyxl
-
-#LEER excel
-
-archivo_excel = pd.read_excel("supermarket_sales.xlsx")
-print(archivo_excel[[""]])
